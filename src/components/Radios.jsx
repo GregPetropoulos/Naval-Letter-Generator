1f16253cd4de25017eeaa8999c03118b364550f3
GP <gregpetropoulos@yahoo.com>
2022-06-15 06:39:45 -0400

    Finished the last UX radio button  in the group.

--- src/components/Radios.jsx
+++ src/components/Radios.jsx
@@ -3,15 +3,15 @@ import { Fragment, useState } from 'react';
 const Radios = () => {
   const [viaIsChecked, setViaIsChecked] = useState(false);
   const [refIsChecked, setRefIsChecked] = useState(false);
+  const [encIsChecked, setEncIsChecked] = useState(false);
 
   const [addViaInput, setAddViaInput] = useState([]);
   const [addRefInput, setAddRefInput] = useState([]);
-  console.log('addViaInput--->STATE', addViaInput);
-  console.log('addRefInpu--->STATE', addRefInput);
+  const [addEncInput, setAddEncInput] = useState([]);
 
   // If the VIA radio button is checked Yes, show the first input field and add button
   // If an additional VIA field is added show the remove button
-// * HANDLING THE RADIO CLICK AND SETTING THE INTIAL STATE
+  // * HANDLING THE RADIO CLICK AND SETTING THE INTIAL STATE
   const viaOnChange = () => {
     setViaIsChecked(!viaIsChecked);
     setAddViaInput([{ via: '' }]);
@@ -21,12 +21,20 @@ const Radios = () => {
     setAddRefInput([{ references: '' }]);
   };
 
+  const encOnChange = () => {
+    setEncIsChecked(!encIsChecked);
+    setAddEncInput([{ enclosures: '' }]);
+  };
+
   // * HANDLING THE ADDITION OF HTML INPUT ELEMENTS
   const addViaInputTag = () => {
     setAddViaInput((prev) => [...prev, { via: '' }]);
   };
   const addRefInputTag = () => {
     setAddRefInput((prev) => [...prev, { references: '' }]);
+  };
+  const addEncInputTag = () => {
+    setAddEncInput((prev) => [...prev, { enclosures: '' }]);
   };
 
   // * HANDLING THE REMOVAL OF HTML INPUT ELEMENTS
@@ -41,6 +49,11 @@ const Radios = () => {
     const inputs = [...addRefInput];
     const arrItemRemoved = inputs.filter((item, idx) => idx !== index);
     setAddRefInput(arrItemRemoved);
+  };
+  const removeEncInputTag = (e, index) => {
+    const inputs = [...addEncInput];
+    const arrItemRemoved = inputs.filter((item, idx) => idx !== index);
+    setAddEncInput(arrItemRemoved);
   };
 
   // * HADLING THE ONCHANGES FOR TEXT INPUTS SEPERATELY
@@ -61,11 +74,20 @@ const Radios = () => {
     setAddRefInput(textRefInputs);
   };
 
+  const handleEncTextInput = (e, index) => {
+    const textEncInputs = [...addEncInput];
+    const { name, value } = e.target;
+    textEncInputs[index][name] = value;
+    setAddEncInput(textEncInputs);
+  };
+
   return (
     <Fragment>
       <label className='sm:text-xl mt-7'> Optional Items</label>
-
+      
+      {/****--------------1st RADIO BUTTON AND FIELDS-----------------****/}
       <div className='form-control block items-center text-xs my-1 sm:text-lg'>
+
         <label className='mb-2 mr-2'> Do you have a VIA?</label>
         <input
           type='radio'
@@ -122,7 +144,8 @@ const Radios = () => {
             </div>
           ))
         : null}
-      {/*!* Stopped here need to cop[y and paste therest in below here and update the youtube comment---------------2nd RADIO BUTTON AND FIELDS----------------- */}
+
+      {/****--------------2nd RADIO BUTTON AND FIELDS-----------------****/}
       <div className='form-control block items-center text-xs my-1 sm:text-lg'>
         <label className='mb-2 mr-2'> Do you have References</label>
         <input
@@ -180,40 +203,64 @@ const Radios = () => {
             </div>
           ))
         : null}
-
-      {/* <div className='form-control block text-xs my-1 sm:text-lg'>
-        <label className='mb-2 mr-2'> Do you have References?</label>
+      {/****--------------3nd RADIO BUTTON AND FIELDS-----------------****/}
+      <div className='form-control block items-center text-xs my-1 sm:text-lg'>
+        <label className='mb-2 mr-2'> Do you have Enclosures</label>
         <input
           type='radio'
-          name='radio-2'
-          className='radio radio-primary mx-2 radio-xs checked:bg-blue-500 sm:radio-lg'
+          name='radio-3'
+          id='radio-3'
+          value='3'
+          checked={encIsChecked === true}
+          onChange={encOnChange}
+          className='radio radio-primary mx-2 radio-xs checked:bg-blue-500 sm:radio-lg sm:m-0'
         />{' '}
         Yes
         <input
           type='radio'
-          name='radio-2'
+          name='radio-3'
+          id='radio-3'
+          value='3'
+          checked={encIsChecked === false}
+          onChange={encOnChange}
           className='radio  mx-2 radio-xs radio-primary checked:bg-red-500 sm:radio-lg'
-          checked
         />
         No
       </div>
+      {encIsChecked
+        ? addEncInput.map((item, index) => (
+            <div key={index}>
+              <input
+                name='enclosures'
+                id='enclosures'
+                value={item.enclosures}
+                onChange={(e) => handleEncTextInput(e, index)}
+                type='text'
+                className='block w-full text-black text-[8px] my-3 rounded-md py-2 pl-1 pr-2 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-base sm:pl-9 sm:pr-3'
+                placeholder='Enter The Via (Title, name of activity (Code), location when needed'
+              />
 
-      <div className='form-control block text-xs my-1 sm:text-lg'>
-        <label className='mb-2 mr-2'> Do you have Enclosures?</label>
-        <input
-          type='radio'
-          name='radio-3'
-          className='radio radio-primary mx-2 radio-xs checked:bg-blue-500 sm:radio-lg'
-        />{' '}
-        Yes
-        <input
-          type='radio'
-          name='radio-3'
-          className='radio  mx-2 radio-xs radio-primary checked:bg-red-500 sm:radio-lg'
-          checked
-        />
-        No
-      </div> */}
+              {/* Add button appears on last input field added */}
+              {addEncInput.length - 1 === index && addEncInput.length < 3 && (
+                <button
+                  type='button'
+                  className='btn mr-4 btn-sm sm:btn'
+                  onClick={addEncInputTag}>
+                  Add Encl
+                </button>
+              )}
+
+              {addEncInput.length > 1 && (
+                <button
+                  type='button'
+                  className='btn btn-sm sm:btn'
+                  onClick={(e) => removeEncInputTag(e, index)}>
+                  Remove Encl
+                </button>
+              )}
+            </div>
+          ))
+        : null}
     </Fragment>
   );
 };
